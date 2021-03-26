161a0b1e5591c12aaf04bfd7db6ee3f54453d1ca
Ziv Kaplan <zivkaplan@gmail.com>
2021-03-27 02:42:47 +0300

    version 1: console interaction only update

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
-
+//computer player object
 function Computer() {
     this.play = () => (Math.floor(Math.random() * 3) + 1)
     this.score = 0;
 }
+const computer = new Computer();
+
+//user player object
 function Player() {
     this.choose = () => (window.prompt("Rock, paper or scissors?").toLowerCase())
     this.score = 0;
 }
-
-const computer = new Computer();
 const player = new Player();
-const moves = { 1: "rock", 2: "paper", 3: "scissors" }
 
-
+//validating user input
 function validate(input) {
     const reversedMoves = { "rock": 1, "paper": 2, "scissors": 3 }
     return reversedMoves[input]
 }
-function check(comp, user) {
+
+//functiom that checks who won
+function checkWinner(comp, user) {
+    const moves = { 1: "rock", 2: "paper", 3: "scissors" }
     if (comp === user) {
         return `draw!\nComputer chose ${moves[comp]}\nYou chose ${moves[user]}`
     }
@@ -29,11 +32,14 @@ function check(comp, user) {
     return `You Lost\nComputer chose ${moves[comp]}\nYou chose ${moves[user]}`
 }
 
+//Game
 for (let i = 0; i < 5; i++) {
     let userInput = false;
     while (!userInput) {
         userInput = validate(player.choose())
     }
-    console.log(check(computer.play(), userInput));
+    console.log(checkWinner(computer.play(), userInput));
 }
+
+//prints final score
 console.log(`Computer score: ${computer.score}\nPlayer score: ${player.score}`);
